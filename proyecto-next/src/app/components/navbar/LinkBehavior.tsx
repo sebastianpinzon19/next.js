397c04f7b92ff74bbea5586f417920317b1ad1df
sebastianpinzon19 <152411485+sebastianpinzon19@users.noreply.github.com>
2024-11-22 19:45:19 -0500

    proyecto inicial

---
+++ proyecto-next/src/app/components/navbar/LinkBehavior.tsx
@@ -0,0 +1,19 @@
+import React, {forwardRef} from "react";
+import Link from "next/link";
+
+interface LinkBehaviorProps extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
+    href: string;
+}
+
+
+const LinkBehavior = forwardRef<HTMLAnchorElement, LinkBehaviorProps>(
+    ({href, children, ...rest}, ref) => {
+        return (
+            <Link href={href} passHref>
+                <a ref={ref} {...rest}/>
+            </Link>
+        );
+    }
+)
+
+export default LinkBehavior;
